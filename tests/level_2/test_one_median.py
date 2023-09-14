7a15cfc4dceea85f6d1ccb39133d51bbbadcc292
Mischa <mishayartsev@gmail.com>
2023-09-14 18:52:43 +0300

    +one_median test

--- tests/level_2/test_one_median.py
+++ tests/level_2/test_one_median.py
@@ -1,25 +1,25 @@
 from functions.level_2.one_median import get_median_value
 
 
-def test__get_median_value_if_empty_list():
+def test__get_median_value__if_items_list_is_empty():
     test_list = []
 
     assert get_median_value(test_list) is None
 
 
-def test__get_median_value_list_with_one_value():
+def test__get_median_value__items_consist_one_value():
     test_list = [11]
 
     assert get_median_value(test_list) == 11
 
 
-def test__get_median_value_check_even_list():
-    test_list = [11, 9, 3, 5, 5, 2, 7, 1, 8]
+def test__get_median_value__items_list_of_odd_numbers():
+    test_list = [11, 9, 3, 5, 7, 2, 7, 1, 8]
 
-    assert get_median_value(test_list) is None
+    assert get_median_value(test_list) == 6
 
 
-def test__get_median_value_check_odd_list():
-    test_list = [11, 9, 3, 5, 5, 2, 7, 1, 8]
+def test__get_median_value__items_list_of_even_numbers():
+    test_list = [11, 9, 3, 5, 7, 2, 7, 1]
 
-    assert get_median_value(test_list) is None
+    assert get_median_value(test_list) == 7
