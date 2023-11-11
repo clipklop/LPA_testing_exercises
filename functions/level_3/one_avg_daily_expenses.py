5df5f21afcd54ee5a286debd911fcc14d8c0eb94
Mischa <mishayartsev@gmail.com>
2023-11-11 13:14:02 +0300

    wip 1 module

--- functions/level_3/one_avg_daily_expenses.py
+++ functions/level_3/one_avg_daily_expenses.py
@@ -2,11 +2,27 @@ import collections
 import decimal
 from statistics import mean
 
-from functions.level_3.models import Expense
+# from functions.level_3.models import Expense
+from models import Expense
 
 
 def calculate_average_daily_expenses(expenses: list[Expense]) -> decimal.Decimal:
     total_expenses_dy_day = collections.defaultdict(decimal.Decimal)
     for expense in expenses:
-        total_expenses_dy_day[expense.spent_at.date()] += expense.amount
+        print(expense.spent_at)
+        # total_expenses_dy_day[expense.spent_at.date()] += expense.amount
     return mean(total_expenses_dy_day.values())
+
+import datetime
+expenses = {
+    'amount': 10,
+    'currency': 'USD',
+    'card': {
+        'last_digits': '4849',
+        'owner': 'Vasek'
+    },
+    'spent_in': 'Somewhere',
+    'spent_at': datetime.datetime(2023, 11, 11, 13, 7, 5, 533855),
+    'category': 'BAR_RESTAURANT',
+}
+print(calculate_average_daily_expenses(expenses))
