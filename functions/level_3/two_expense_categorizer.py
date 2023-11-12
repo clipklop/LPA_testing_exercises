c00369f30e2c7d0e468441cf7989dd96b18a929f
Mischa <mishayartsev@gmail.com>
2023-11-12 23:58:31 +0300

    wip second challenge

--- functions/level_3/two_expense_categorizer.py
+++ functions/level_3/two_expense_categorizer.py
@@ -1,4 +1,5 @@
-from functions.level_3.models import Expense, ExpenseCategory
+# from functions.level_3.models import Expense, ExpenseCategory
+from models import Expense, ExpenseCategory
 
 
 def guess_expense_category(expense: Expense) -> ExpenseCategory | None:
@@ -12,6 +13,7 @@ def guess_expense_category(expense: Expense) -> ExpenseCategory | None:
     }
     for possible_category, triggers in triggers_map.items():
         for trigger in triggers:
+            print(trigger)
             if is_string_contains_trigger(expense.spent_in, trigger):
                 return possible_category
     return None
@@ -30,3 +32,10 @@ def is_string_contains_trigger(original_string: str, trigger: str) -> bool:
                 for p in allowed_delimiters for d in allowed_delimiters
             )
     )
+
+import datetime
+print(guess_expense_category(
+Expense(amount=10, currency='USD', card={'last_digits': '4849', 'owner': 'Vasek'},
+            spent_in='ONLINE_SUBSCRIPTIONS', spent_at=datetime.datetime(2023, 11, 11, 13, 7, 5, 533855),
+            category='BAR_RESTAURANT')
+))
